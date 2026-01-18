5f4ff72b4d70ef5a1800fa7a4251523e1068594a
linarifux <linarifux@gmail.com>
2026-01-19 01:54:40 +0600

    index route added in the server.js file, readme file udpated

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import morgan from 'morgan';
 import cors from 'cors';
-import cookieParser from 'cookie-parser'; // Added for JWT cookies
+import cookieParser from 'cookie-parser'; 
 import connectDB from './config/db.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
 
@@ -30,12 +30,17 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser()); // Parses cookies for auth
 
+// index route
+app.get('/', (req, res) => {
+  res.send('API is Running...')
+})
+
 // API Routes
 app.use('/api/appointments', appointmentRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
 
-// Custom Error Middleware (Must be after routes)
+// Custom Error Middleware 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
